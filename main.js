9f6c6968d7e1d783b72cc057c8f8fc5059b91446
LingkonDash <cocrupon@gamil.com>
2026-02-14 21:03:17 +0600

    BD flag

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ let thatDibba = document.getElementById("dibba");
 
 let square = document.createElement("div");
 square.classList.add("square")
-console.log(square);
-
 thatDibba.appendChild(square)
+let gulMotor = document.createElement("div");
+gulMotor.classList.add("motor")
+thatDibba.appendChild(gulMotor)
 
-document.addEventListener("mousemove", () => {
-  let mouseX = event.clientX;
-  let mouseY = event.clientY;
-  console.log(mouseX, mouseY);
-  
+document.addEventListener("mousemove", (e) => {
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
 
   square.style.transform = `translate(calc(${mouseX}px - 100px), calc(${mouseY}px - 100px))`
+  gulMotor.style.transform = `translate(calc(${mouseX}px - 50px), calc(${mouseY}px - 50px))`
 })
