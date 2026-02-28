cd38643e06dfbd8b13fa9db43d160e7f5b5ee74e
LingkonDash <cocrupon@gamil.com>
2026-03-01 02:36:50 +0600

    counter project

--- practice-2/counter.js
+++ practice-2/counter.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+const container = document.getElementById('container');
+const number = document.getElementById('number');
+
+container.addEventListener('click', (e) => {
+
+  let number =Number(document.getElementById('number').innerText);
+  
+  if(e.target.classList.contains('increase')) {
+    document.getElementById('number').innerText = ++number;
+  }
+  if(e.target.classList.contains('decrease')) {
+    document.getElementById('number').innerText = --number;
+  }
+  if(e.target.classList.contains('reset')) {
+    document.getElementById('number').innerText = '0';
+  }
+  
+})
+
